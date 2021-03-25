3205888431fcfcffaf9981fcb25bac9de392d9ad
Matt Priour <mpriour@esri.com>
2021-03-25 10:01:26 -0500

    Final for exercise 6 - Authentication

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,13 +1,25 @@
+import * as auth from '../auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, customConfig = {}) {
+function client(endpoint, {token, data, headers, ...customConfig} = {}) {
   const config = {
-    method: 'GET',
+    method: (data) ? 'POST' : 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': (data) ? 'application/json' : undefined,
+      ...headers,
+    },
+    body: (data) ? JSON.stringify(data) : undefined,
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+    if(response.status === 401){
+      await auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate'})
+    }
     if (response.ok) {
       return data
     } else {
